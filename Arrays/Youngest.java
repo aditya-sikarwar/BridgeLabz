3d54461ce4646c6f35838c0230590c8c3214a474
Aditya Sikarwar <aditya.sikarwar_cs.aiml22@gla.ac.in>
2025-06-25 14:46:29 +0530

    Youngest.java modification

--- Arrays/Youngest.java
+++ Arrays/Youngest.java
@@ -1,31 +1,49 @@
 package day3;
 import java.util.*;
 
-public class Youngest{
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		
-		System.out.println("Enter height: ");
-		int []Height=new int[3];
-		for(int i=0; i<3; i++) {
-			Height[i]=sc.nextInt();
-		}
-		
-		System.out.println("Enter age: ");
-		int [] age= new int[3];
-		for(int i=0; i<3; i++) {
-			age[i]=sc.nextInt();
-		}
-		int youngest=0;
-		for(int i=0; i<3; i++) {
-			if(age[i]<age[i+1]) {
-			youngest=age[i];
-			}
-			else {
-				youngest=age[i+1];
-			}
-		}
-		System.out.println("youngest in age is: " + youngest);
-		
-	}
+public class Youngest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] names = {"Amar", "Akbar", "Anthony"};
+
+        int[] height = new int[3];
+        int[] age = new int[3];
+
+        
+        System.out.println("Enter height for Amar, Akbar, Anthony:");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Height of " + names[i] + ": ");
+            height[i] = sc.nextInt();
+        }
+
+        
+        System.out.println("\nEnter age for Amar, Akbar, Anthony:");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Age of " + names[i] + ": ");
+            age[i] = sc.nextInt();
+        }
+
+        
+        int youngestIndex = 0;
+        for (int i = 1; i < 3; i++) {
+            if (age[i] < age[youngestIndex]) {
+                youngestIndex = i;
+            }
+        }
+
+    
+        int tallestIndex = 0;
+        for (int i = 1; i < 3; i++) {
+            if (height[i] > height[tallestIndex]) {
+                tallestIndex = i;
+            }
+        }
+
+        
+        System.out.println("\nYoungest friend: " + names[youngestIndex] + " (Age: " + age[youngestIndex] + ")");
+        System.out.println("Tallest friend: " + names[tallestIndex] + " (Height: " + height[tallestIndex] + " cm)");
+
+        
+    }
 }
